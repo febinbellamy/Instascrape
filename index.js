12762d451635f0d5123ddc69b0d7386862769012
Febin Bellamy <febinbellamy@gmail.com>
2022-10-28 13:46:29 -0400

    Bulk updating rows in sheet

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const puppeteer = require("puppeteer");
 const credentials = require("./credentials");
+const Sheet = require("./sheet");
 
 (async () => {
   const browser = await puppeteer.launch({ headless: false });
@@ -28,15 +29,18 @@ const credentials = require("./credentials");
     "duranthighlights",
   ];
 
-  // this loop will visit each one of the usernames' pages and scrape them
+  const profiles = [];
+
   for (let i = 0; i < usernames.length; i++) {
     let username = usernames[i];
     await page.goto(`https://instagram.com/${username}`);
     await page.waitForSelector("img");
+    
+    await page.waitForTimeout(3000);
 
     const profilePicSrc = await page
       .$eval("img", (element) => element.getAttribute("src"))
-      .catch((e) => console.log(e));
+      .catch(() => false);
 
     const stats = await page.$$eval("header li", (elements) =>
       elements.map((element) => element.textContent)
@@ -44,42 +48,48 @@ const credentials = require("./credentials");
 
     const fullName = await page
       .$eval("._aa_c span", (element) => element.textContent)
-      .catch((e) => console.log(e));
+      .catch(() => false);
 
     const bio = await page
       .$eval(
         "._aa_c ._aacl._aacp._aacu._aacx._aad6._aade",
         (element) => element.textContent
       )
-      .catch((e) => console.log(e));
+      .catch(() => false);
 
     const profileLink = await page
       .$eval(
         "._aacl._aacp._aacw._aacz._aada._aade",
         (element) => element.textContent
       )
-      .catch((e) => console.log(e));
+      .catch(() => false);
 
-    // these are the row names for the spreadsheet
     const profile = {
       profilePicSrc,
-      stats,
       fullName,
       bio,
       profileLink,
-      username,
+      username: username,
     };
 
-    // splits the stats data and puts it into the profile object
-    for (let i = 0; i < stats.length; i++) {
-      let stat = stats[i];
+    for (let stat of stats) {
       const [count, name] = stat.split(" ");
       profile[name] = count;
     }
-
-    console.log({ profile });
-
+    profiles.push(profile);
   }
 
-  // await browser.close();
+  const sheet = new Sheet();
+  await sheet.load();
+
+  const prevProfiles = await sheet.getRows(0);
+  for (let prevProfile of prevProfiles) {
+    if (usernames.includes(prevProfile.username)) {
+      await prevProfile.delete();
+    }
+  }
+
+  await sheet.addRows(profiles, 0);
+
+  await browser.close();
 })();
